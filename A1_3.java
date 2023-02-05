beabca8c7a38f70b8f03f582d5c19f3beb91ffc0
Vinay Ku Garg <gargv124@gmail.com>
2023-02-05 16:07:20 +0530

    Vinay Gandu

---
+++ A1_3.java
@@ -0,0 +1,10 @@
+package Anuj_sir_Assignment;
+class A1_3
+{
+	public static void main(String[] args)
+	{
+		String str1="vinay";
+		String str2="apple";
+		System.out.println(str1.length() == str2.length());
+	}
+}
